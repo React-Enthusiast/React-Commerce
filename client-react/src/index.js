f24fb2b38bcf4812cf3278821d47d46c5bd3ccbd
safarudinyoga <safarudinyoga@gmail.com>
2019-11-04 01:16:55 +0700

    adding redux,thunk and redux-middleware

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './reducers';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import Ecommerce from './components/Ecommerce';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <Ecommerce />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
