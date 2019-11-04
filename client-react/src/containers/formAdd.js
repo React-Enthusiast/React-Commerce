1855870ae28427e03c1fc15967e8ffdb2fbc4d76
safarudinyoga <safarudinyoga@gmail.com>
2019-11-04 18:56:40 +0700

    add form complete

--- client-react/src/containers/formAdd.js
+++ client-react/src/containers/formAdd.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux';
-import { addData } from '../actions'
+import { addData } from '../actions';
+import { SketchPicker } from 'react-color';
 
 class FormAdd extends React.Component {
     constructor(props) {
@@ -14,14 +15,16 @@ class FormAdd extends React.Component {
             vote: '',
             testimonials: '',
             rate: '',
-            colors: '',
-            capacities: ''
+            colors: '#fff',
+            capacities: '',
+            displaypicker: false,
         }
 
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
-        // this.handleButtonAdd = this.handleButtonAdd.bind(this)
-        // this.handleButtonCancel = this.handleButtonCancel.bind(this)
+        this.handleButtonColor = this.handleButtonColor.bind(this)
+        this.handleColorClose = this.handleColorClose.bind(this)
+        this.handleChangeColor = this.handleChangeColor.bind(this)
     }
 
     handleChange(event) {
@@ -53,14 +56,70 @@ class FormAdd extends React.Component {
             vote: '',
             testimonials: '',
             rate: '',
-            colors: '',
+            colors: '#fff',
             capacities: ''
         })
     }
 
+    handleButtonColor() {
+        this.setState({
+            displaypicker: true
+        })
+    }
+
+    handleColorClose = (event) => {
+        console.log(event.target);
+        let { id } = event.target
+        this.setState({
+            displaypicker: false
+
+        })
+    }
+
+    handleChangeColor = (color, event) => {
+        this.setState({
+            // colors: event.target.colors,
+            colors: color.hex,
+            displaypicker: true
+        })
+    }
+
+    colorPicker() {
+        if (this.state.displaypicker) {
+            return (
+                <div className="form-group row" >
+                    <label htmlFor="colors" className="col-sm-2 col-form-label">Colors</label>
+                    <div className="col-4">
+                        <SketchPicker color={this.state.colors} onChange={this.handleChangeColor} />
+                    </div>
+                    <div className="col-5" onClick={this.handleColorClose}></div>
+                </div>
+            )
+        } else {
+            return (
+                <div className="form-group row">
+                    <label htmlFor="colors" className="col-sm-2 col-form-label">Colors</label>
+                    <div className="col-1">
+                        <div style={{
+                            width: '40px',
+                            height: '15px',
+                            marginTop: '10px',
+                            borderRadius: '2px',
+                            backgroundColor: this.state.colors
+                        }}>
+                        </div>
+                    </div>
+                    <div className="col-1">
+                        <button type="button" onClick={this.handleButtonColor} className="btn btn-info"><i className="fa fa-plus" aria-hidden="true"></i></button>
+                    </div>
+                </div>
+            )
+        }
+    }
+
     render() {
         return (
-            <form>
+            <form >
                 <div className="form-group row">
                     <label htmlFor="title" className="col-sm-2 col-form-label">Title</label>
                     <div className="col-sm-10">
@@ -70,7 +129,7 @@ class FormAdd extends React.Component {
                 <div className="form-group row">
                     <label htmlFor="description" className="col-sm-2 col-form-label">Description</label>
                     <div className="col-sm-10">
-                        <textarea type="text" className="form-control" id="description" name="description" rows="5"/>
+                        <textarea type="text" className="form-control" id="description" name="description" rows="5" />
                     </div>
                 </div>
                 <div className="form-group row">
@@ -88,7 +147,7 @@ class FormAdd extends React.Component {
                 <div className="form-group row">
                     <label htmlFor="detail" className="col-sm-2 col-form-label">Detail Product</label>
                     <div className="col-sm-10">
-                        <textarea type="text" className="form-control" id="detail" name="detail" rows="5"/>
+                        <textarea type="text" className="form-control" id="detail" name="detail" rows="5" />
                     </div>
                 </div>
                 <div className="form-group row">
@@ -100,7 +159,7 @@ class FormAdd extends React.Component {
                 <div className="form-group row">
                     <label htmlFor="testimonials" className="col-sm-2 col-form-label">Testimonials Product</label>
                     <div className="col-sm-10">
-                        <textarea type="text" className="form-control" id="testimonials" name="testimonials" rows="5"/>
+                        <textarea type="text" className="form-control" id="testimonials" name="testimonials" rows="5" />
                     </div>
                 </div>
                 <div className="form-group row">
@@ -109,16 +168,49 @@ class FormAdd extends React.Component {
                         <input type="number" className="form-control" id="rate" name="rate" />
                     </div>
                 </div>
-                <div className="form-group row">
+                <div>
+                    {this.colorPicker()}
+                </div>
+                {/* <div>{this.colorPicker}</div> */}
+                {/* <div className="form-group row">
                     <label htmlFor="colors" className="col-sm-2 col-form-label">Colors</label>
                     <div className="col-2">
-                        <input type="color" className="form-control" id="colors" name="colors" />
+                        <input onClick={this.handleButtonColor} className="form-control" id="colors" name="colors" />
+                        <button type="button" onClick={this.handleButtonColor} className="btn btn-info"><i className="fa fa-plus" aria-hidden="true"></i></button>
+                        <SketchPicker />
                     </div>
-                </div>
+                </div> */}
                 <div className="form-group row">
                     <label htmlFor="capacities" className="col-sm-2 col-form-label">Capacities</label>
-                    <div className="col-sm-10">
-                        <input type="number" className="form-control" id="capacities" name="capacities" />
+                    <div className="col-sm-2">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="capacities" />
+                            <label className="custom-control-label" htmlFor="capacities">16 Gb</label>
+                        </div>
+                    </div>
+                    <div className="col-sm-2">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="capacities1" />
+                            <label className="custom-control-label" htmlFor="capacities1">32 Gb</label>
+                        </div>
+                    </div>
+                    <div className="col-sm-2">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="capacities2" />
+                            <label className="custom-control-label" htmlFor="capacities2">64 Gb</label>
+                        </div>
+                    </div>
+                    <div className="col-sm-2">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="capacities3" />
+                            <label className="custom-control-label" htmlFor="capacities3">128 Gb</label>
+                        </div>
+                    </div>
+                    <div className="col-sm-2">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="capacities4" />
+                            <label className="custom-control-label" htmlFor="capacities4">256 Gb</label>
+                        </div>
                     </div>
                 </div>
                 <div className="form-group row">
@@ -130,11 +222,11 @@ class FormAdd extends React.Component {
                         </div>
                     </div>
                 </div>
-                <div className="form-group col-md-4">
+                <div className="form-group col-md-4 my-5">
                     <button type="button" className="btn btn-success mr-2"><i className="fas fa-check"></i> Save</button>
                     <button type="reset" className="btn btn-danger"> <i className="fas fa-undo"></i> Cancel</button>
                 </div>
-            </form>
+            </form >
         )
     }
 
