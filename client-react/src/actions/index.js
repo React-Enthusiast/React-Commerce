24ab3669bf19fc67dea2b73172f4f9e1b1fa59fa
adscript <adnanocode@gmail.com>
2019-11-06 04:41:45 +0700

    added load product, load detail, rate, and vote

--- client-react/src/actions/index.js
+++ client-react/src/actions/index.js
@@ -1,15 +1,25 @@
 import {
-    LOAD_DATA_SUCCESS,
-    LOAD_DATA_FAILURE,
+    LOAD_PRODUCT_SUCCESS,
+    LOAD_PRODUCT_FAILURE,
+
+    LOAD_DETAIL_FAILURE,
+    LOAD_DETAIL_SUCCESS,
+
     ADD_DATA,
     ADD_DATA_SUCCESS,
     ADD_DATA_FAILURE,
-    DELETE_DATA,
-    DELETE_DATA_SUCCESS,
-    DELETE_DATA_FAILURE
+    
+    ADD_VOTE,
+    ADD_VOTE_SUCCESS,
+    ADD_VOTE_FAILURE,
+    
+    ADD_RATE,
+    ADD_RATE_SUCCESS,
+    ADD_RATE_FAILURE,
 } from "../constants/actiontype"
 
 import axios from 'axios'
+import Swal from "sweetalert2";
 
 const API_URL = 'http://localhost:3001/api/'
 
@@ -18,16 +28,178 @@ const request = axios.create({
     timeout: 1000
 });
 
-export const addDataSuccess = () => ({
+// START LOAD ALL PRODUCTS
+const loadProductSuccess = (products) => {
+    return { type: LOAD_PRODUCT_SUCCESS, products }
+}
+
+const loadProductFailure = () => {
+    return { type: LOAD_PRODUCT_FAILURE }
+}
+
+export const loadProduct = (fetch) => {
+    return dispatch => {
+        return request.get(`products/${fetch}`)
+            .then(function (response) {
+                if (response.data.status === 'SUCCESS')
+                    dispatch(loadProductSuccess(response.data.productData));
+                else{
+                    dispatch(loadProductFailure());
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Connection Lost',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+                dispatch(loadProductFailure());
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Connection Lost',
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            })
+    }
+}
+
+// START LOAD DETAIL
+const loadProductDetailSuccess = (product) => {
+    return { type: LOAD_DETAIL_SUCCESS, product }
+}
+
+const loadProductDetailFailure = () => {
+    return { type: LOAD_DETAIL_FAILURE }
+}
+
+export const loadProductDetail = (id) => {
+    return dispatch => {
+        return request.get(`products/detail/${id}`)
+            .then(function (response) {
+                if (response.data.status === 'SUCCESS')
+                    dispatch(loadProductDetailSuccess(response.data.productData));
+                else{
+                    dispatch(loadProductDetailFailure());
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Connection Lost',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+                dispatch(loadProductDetailFailure());
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Connection Lost',
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            })
+    }
+}
+
+// START ADD VOTE
+const voteProductRedux = (id, lastVoteSum) => {
+    return { type : ADD_VOTE, id, vote : lastVoteSum }
+}
+
+const voteProductSuccess = () => {
+    return { type : ADD_VOTE_SUCCESS }
+}
+
+const voteProductFailure = () => {
+    return { type : ADD_VOTE_FAILURE }
+}
+
+export const voteProduct = (id, lastVoteSum) => {
+    return dispatch => {
+        dispatch(voteProductRedux(id, lastVoteSum));
+        return request.put(`products/${id}`)
+        .then(function (response) {
+            if (response.data.status === 'SUCCESS')
+                dispatch(voteProductSuccess(id, lastVoteSum));
+            else
+                dispatch(voteProductFailure());
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Connection Lost',
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+        })
+        .catch(function (error) {
+            console.log(error);
+            dispatch(voteProductFailure());
+            Swal.fire({
+                icon: 'error',
+                title: 'Connection Lost',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        })
+    }
+}
+
+// START ADD RATE
+const rateProductRedux = (id, lastRate, newTestimonial) => {
+    return { type : ADD_RATE, id, rate: lastRate, testimonials: newTestimonial }
+}
+
+const rateProductSuccess = () => {
+    return { type : ADD_RATE_SUCCESS };
+}
+
+const rateProductFailure = () => {
+    return { type : ADD_RATE_FAILURE };
+}
+
+export const rateProduct = (id, lastRate, newTestimonial) => {
+    return dispatch => {
+        dispatch(rateProductRedux(id, lastRate, newTestimonial));
+        return request.put(`products/${id}`)
+        .then(function (response) {
+            if (response.data.status === 'SUCCESS')
+                dispatch(rateProductSuccess());
+            else{
+                dispatch(rateProductFailure());
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Connection Lost',
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+            dispatch(rateProductFailure());
+            Swal.fire({
+                icon: 'error',
+                title: 'Connection Lost',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        })
+    }
+}
+
+// START ADD PRODUCT
+const addDataSuccess = () => ({
     type: ADD_DATA_SUCCESS
 })
 
-export const addDataFailure = (id) => ({
+const addDataFailure = (id) => ({
     type: ADD_DATA_FAILURE,
     id
 })
 
-export const addDataRedux = (id, title, description, brand, price, detail, vote, testimonial, rate, colors, capacities) => ({
+const addDataRedux = (id, title, description, brand, price, detail, vote, testimonial, rate, colors, capacities) => ({
     type: ADD_DATA,
     id,
     title,
@@ -47,10 +219,12 @@ export const addData = (title, description, brand, price, detail, vote, testimon
     return dispatch => {
         dispatch(addDataRedux(id, title, description, brand, price, detail, vote, testimonial, rate, colors, capacities))
         return request.post('', { id, title, description, brand, price, detail, vote, testimonial, rate, colors, capacities })
-        .then(response => {
-            dispatch(addDataSuccess(response.data))
-        }).catch(error => {
-            dispatch(addDataFailure(id))
-        })
+            .then(response => {
+                dispatch(addDataSuccess(response.data))
+            }).catch(error => {
+                dispatch(addDataFailure(id))
+            })
     }
 }
+
+
