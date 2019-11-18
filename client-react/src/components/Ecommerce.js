249d2efdffffe5eefbc6a944db63411e7a6eff3c
safarudinyoga <safarudinyoga@gmail.com>
2019-11-18 20:49:40 +0700

    updated

--- client-react/src/components/Ecommerce.js
+++ client-react/src/components/Ecommerce.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import FormAdd from '../containers/addForm/formAdd'
-
-import Upload from '../containers/addForm/upload';
-import Dropzone from '../containers/addForm/dropzone'
-
+import ListProduct from '../containers/listProduct'
 export default class Ecommerce extends React.Component {
 
     render() {
         return (
-            <div className="container my-5">
-                <div className="card">
-                    <div className="card-header text-center">
-                        <h2>Form Add</h2>
-                    </div>
-                    <div className="card-body">
-                        <FormAdd />
-                    </div>
+            <div className="container-fluid ">
+                <div className="card card-responsive-width" style={{ padding: '5vh' }}>
+                    <FormAdd />
                 </div>
             </div>
         )
