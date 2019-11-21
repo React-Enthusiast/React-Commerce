f49c0a7ab3955db8d08c978107344520159fa35b
safarudinyoga <safarudinyoga@gmail.com>
2019-11-22 02:33:00 +0700

    Change addForm to Object

--- client-react/src/reducers/product.js
+++ client-react/src/reducers/product.js
@@ -7,41 +7,42 @@ import {
     ADD_DATA_FAILURE,
 
 } from "../constants/actiontype"
-
-const products = (state = [], action) => {
+// change state to be an object
+const products = (state = {}, action) => {
 
     switch (action.type) {
         case LOAD_PRODUCT_SUCCESS:
             return action.products
 
-        case ADD_DATA:
-            return [
-                ...state,
-                {
-                    id: action.id,
-                    title: action.title,
-                    description: action.description,
-                    brand: action.brand,
-                    price: action.price,
-                    detail: action.detail,
-                    colors: action.colors,
-                    capacities: action.capacities,
-                    filename: action.filename
-                }
-            ]
-
         // case ADD_DATA:
-        //     return Object.assign({}, state, {
-        //         id: action.id,
-        //         title: action.title,
-        //         description: action.description,
-        //         brand: action.brand,
-        //         price: action.price,
-        //         detail: action.detail,
-        //         colors: action.colors,
-        //         capacities: action.capacities,
-        //         filename: action.filename
-        //     })
+        //     return [
+        //         ...state,
+        //         {
+        //             id: action.id,
+        //             title: action.title,
+        //             description: action.description,
+        //             brand: action.brand,
+        //             price: action.price,
+        //             detail: action.detail,
+        //             colors: action.colors,
+        //             capacities: action.capacities,
+        //             filename: action.filename
+        //         }
+        //     ]
+
+        // change state to object type
+        case ADD_DATA:
+            return Object.assign({}, state, {
+                id: action.id,
+                title: action.title,
+                description: action.description,
+                brand: action.brand,
+                price: action.price,
+                detail: action.detail,
+                colors: action.colors,
+                capacities: action.capacities,
+                filename: action.filename
+            })
 
         case ADD_DATA_SUCCESS:
             return state.map(data => {
